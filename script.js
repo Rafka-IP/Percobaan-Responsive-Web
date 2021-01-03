c958f052c373b22d3006c66c1c765d7f104f284f
Rafka-IP <68463954+Rafka-IP@users.noreply.github.com>
2021-01-03 13:23:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/* const selectElement = (element) => document.querySelector(element);
+
+const header = selectElement('header');
+const mainContent = selectElement('main');
+
+selectElement('.navbar').addEventListener('click', () => {
+  header.classList.toggle('active');
+  mainContent.classList.toggle('active');
+});
+
+window.onclick = (event) => {
+  if (event.target.matches('.active')) {
+    if (header.classList.contains('active')) {
+      header.classList.remove('active');
+      mainContent.classList.remove('active');
+    }
+  }
+};
+*/
+
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    let windowPosition = window.scrollY > 0;
+    
+    header.classList.toggle("scrolling-active", windowPosition);
+    
+  });
+  
+ 
+  
